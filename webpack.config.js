1b4b6be00858b61ab43cc505d551029e0dcecb99
tobby8629 <sarhmullah15@gmail.com>
2023-01-30 09:06:37 -0800

    clear linter error

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,34 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    mode: 'development',
-    entry:{
-        bundle: path.resolve(__dirname, './src/index.js')
+  mode: 'development',
+  entry: {
+    bundle: path.resolve(__dirname, './src/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name][contenthash].js',
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
     },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name][contenthash].js',
-        clean: true,
-    },
-    devServer: {
-        static : {
-            directory: path.resolve(__dirname, 'dist')
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-    },
-    
-    plugins: [
-        new HtmlWebpackPlugin({
-            template : 'src/template.html',
-            title : 'leader_board',
-        }),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
-}
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/template.html',
+      title: 'leader_board',
+    }),
+  ],
+};
